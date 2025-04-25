a664e9076dbe91a8cbe6ae5ec8f643500675fb37
l.weber <l.weber@traveltrex.com>
2025-04-25 14:56:10 +0200

    add split3 and out

---
+++ out/json.go
@@ -0,0 +1,32 @@
+package out
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func Jsonln(data any) {
+	content, err := json.Marshal(&data)
+	if err != nil {
+		Ejsonln(err)
+		return
+	}
+	fmt.Println(string(content))
+}
+
+func Ejsonln(err error) {
+	type out struct {
+		Error string `json:"error"`
+	}
+	content, err := json.Marshal(&out{Error: err.Error()})
+	if err != nil {
+		Eprintln("ERROR:", err)
+		return
+	}
+	Eprintln(string(content))
+}
+
+func Eprintln(a ...any) {
+	fmt.Fprintln(os.Stderr, a...)
+}
--- strutil/split.go
+++ strutil/split.go
@@ -9,3 +9,14 @@ func Split2(input string, sep string) (string, string) {
 	}
 	return parts[0], parts[1]
 }
+
+func Split3(input string, sep string) (string, string, string) {
+	parts := strings.SplitN(input, sep, 3)
+	if len(parts) == 1 {
+		return parts[0], "", ""
+	}
+	if len(parts) == 2 {
+		return parts[0], parts[1], ""
+	}
+	return parts[0], parts[1], parts[2]
+}
