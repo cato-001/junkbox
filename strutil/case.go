d71848a51f9fecc354fe393f1c4ca3bef334aeb8
l.weber <l.weber@traveltrex.com>
2025-06-09 00:44:44 +0200

    update case conversion

--- strutil/case.go
+++ strutil/case.go
@@ -7,9 +7,47 @@ import (
 	"golang.org/x/text/language"
 )
 
+func CamelCase(input string) string {
+	input = PascalCase(input)
+	return strings.ToLower(string(input[0])) + input[1:]
+}
+
 func PascalCase(input string) string {
 	dashReplacer := strings.NewReplacer("-", " ", "_", " ")
 	caser := cases.Title(language.German, cases.NoLower)
 	spaceReplacer := strings.NewReplacer("\t", "", " ", "")
 	return spaceReplacer.Replace(caser.String(dashReplacer.Replace(input)))
+}
+
+func UpperSnakeCase(input string) string {
+	caser := cases.Upper(language.German)
+	return caser.String(SnakeCase(input))
+}
+
+func SnakeCase(input string) string {
+	return delimited(input, "_")
+}
+
+func KebabCase(input string) string {
+	return delimited(input, "-")
+}
+
+func delimited(input, delimiter string) string {
+	lowercaseLetters := "abcdefghijklmnopqrstuvwxyzöäü"
+	uppercaseLetters := strings.ToUpper(lowercaseLetters)
+
+	replacements := make([]string, len(lowercaseLetters) * 2)
+	for index, lowercaseRune := range lowercaseLetters {
+		lowercaseLetter := string(lowercaseRune)
+		uppercaseLetter := string(uppercaseLetters[index])
+		replacements[index * 2] = uppercaseLetter
+		replacements[index * 2 + 1] = delimiter + lowercaseLetter
+	}
+
+	uppercaseReplacer := strings.NewReplacer(replacements...)
+	spaceReplacer := strings.NewReplacer("-", delimiter, "_", delimiter, "\t", delimiter, " ", delimiter)
+
+	input = uppercaseReplacer.Replace(input)
+	input = spaceReplacer.Replace(input)
+	return strings.Trim(input, "-_\t ")
 }
